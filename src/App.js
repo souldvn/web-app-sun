b8470526de71bec51bfeff6d1b017b855aca802a
souldvn <emilevil97@gmail.com>
2024-11-05 19:22:07 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Splash from './components/splashscreen/Splash';
 import Mainscreen from './components/mainscreen/Mainscreen';
-import Carddeffault from './components/carddeffault/Carddeffault';
 
+const App = () => {
+  const [loading, setLoading] = useState(true); // состояние для загрузки
 
+  useEffect(() => {
+    // Имитация загрузки, например, 3 секунды
+    const timer = setTimeout(() => {
+      setLoading(false); // После 3 секунд устанавливаем loading в false
+    }, 3000);
 
-const tg = window.Telegram.WebApp;
-
-
-function App() {
+    // Очистка таймера при размонтировании
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <div className="App">
-      {/* <Splash /> */}
-      <Mainscreen/>
-      {/* <Carddeffault/> */}
+      {loading ? <Splash /> : <Mainscreen />} {/* Показать Splash или Mainscreen */}
     </div>
   );
 }
 
 export default App;
+

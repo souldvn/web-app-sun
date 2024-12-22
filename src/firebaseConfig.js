d9e2e5f828a5046b5ea5ecef907665fc18426c2b
souldvn <emilevil97@gmail.com>
2024-12-22 16:14:51 +0700

    init

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, get, child } from "firebase/database"; // Убедитесь, что ref импортирован
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDiDo_BNbMyjhYVtmG7EqKAt7FNpyARRXM",
-  authDomain: "sunvill-8fcf7.firebaseapp.com",
-  projectId: "sunvill-8fcf7",
-  storageBucket: "sunvill-8fcf7.appspot.com",
-  messagingSenderId: "340641685680",
-  appId: "1:340641685680:web:635d4005b9eeb762881b75",
-  measurementId: "G-FFBNL9B6FR",
-  databaseURL: "https://sunvill-8fcf7-default-rtdb.europe-west1.firebasedatabase.app"
+  apiKey: "AIzaSyDZfj3T2vSej8mV8Y-xdG21pAAg24DDVe4",
+  authDomain: "sunvillage-6aec8.firebaseapp.com",
+  projectId: "sunvillage-6aec8",
+  storageBucket: "sunvillage-6aec8.firebasestorage.app",
+  messagingSenderId: "117813033135",
+  appId: "1:117813033135:web:5a84e59d4d73fdae037c5a",
+  measurementId: "G-9D6CD5C887",
+  databaseURL: "https://sunvillage-6aec8-default-rtdb.europe-west1.firebasedatabase.app/",
 };
 
 // Initialize Firebase

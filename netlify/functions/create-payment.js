bc8dd5791ec470ed620dfce89ce9a022ed6e22d7
souldvn <emilevil97@gmail.com>
2024-12-24 11:27:16 +0700

    init

--- netlify/functions/create-payment.js
+++ netlify/functions/create-payment.js
@@ -1,22 +1,28 @@
-// netlify/functions/create-payment.js
 const axios = require('axios');
 const { v4: uuidv4 } = require('uuid');
 
-exports.handler = async function(event, context) {
+exports.handler = async (event, context) => {
   const { totalPrice, orderType, comment } = JSON.parse(event.body);
-  const idempotenceKey = uuidv4(); // Генерация уникального Idempotence Key
+  const idempotenceKey = uuidv4();
+
+  if (!idempotenceKey) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Idempotence key is missing.' }),
+    };
+  }
 
   try {
     const response = await axios.post('https://api.yookassa.ru/v3/payments', {
       amount: {
-        value: totalPrice.toFixed(2), // Сумма заказа
+        value: totalPrice.toFixed(2),
         currency: 'RUB',
       },
       capture: true,
       description: `Оплата заказа (${orderType})`,
       confirmation: {
         type: 'redirect',
-        return_url: 'http://yourwebsite.com', // Ваш URL возврата
+        return_url: 'http://google.com', 
       },
       metadata: {
         orderType,
@@ -24,22 +30,21 @@ exports.handler = async function(event, context) {
       },
     }, {
       auth: {
-        username: '1003026', // Ваш shopId
-        password: 'test_OnkvybsCkcuQMqCuArnLlTd-KTGZ-3q1UqetvsnJFo8', // Ваш secretKey
+        username: '1003026',
+        password: 'test_OnkvybsCkcuQMqCuArnLlTd-KTGZ-3q1UqetvsnJFo8',
       },
       headers: {
-        'Idempotence-Key': idempotenceKey, // Передаем ключ идемпотентности
+        'Idempotence-Key': idempotenceKey,
       },
     });
 
     return {
       statusCode: 200,
       body: JSON.stringify({
-        confirmationUrl: response.data.confirmation.confirmation_url, // Ссылка на оплату
+        confirmationUrl: response.data.confirmation.confirmation_url,
       }),
     };
   } catch (error) {
-    console.error('Ошибка при создании платежа:', error.response ? error.response.data : error.message);
     return {
       statusCode: 500,
       body: JSON.stringify({

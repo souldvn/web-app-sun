b97bfc4b1071838d7ff46da57366114f29ce206d
souldvn <emilevil97@gmail.com>
2024-12-24 11:37:22 +0700

    init

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.browser }},
+  pluginReact.configs.flat.recommended,
+];

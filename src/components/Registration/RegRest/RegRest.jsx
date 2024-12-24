756cca8f505978f852beb256414a3dfd4fc676d1
souldvn <emilevil97@gmail.com>
2024-12-24 12:44:57 +0700

    init

--- src/components/Registration/RegRest/RegRest.jsx
+++ src/components/Registration/RegRest/RegRest.jsx
@@ -18,7 +18,7 @@ const RegRest = () => {
       console.log('Idempotence Key:', idempotenceKey); // Логируем сгенерированный ключ
     
       try {
-        const response = await fetch('https://sunvillrest.netlify.app/netlify/functions/payment', {
+        const response = await fetch('https://sunvillrest.netlify.app/.netlify/functions/payment', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',

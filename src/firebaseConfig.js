7506dc63154563575b579bb7f327d70fddc752bd
souldvn <emilevil97@gmail.com>
2025-02-12 09:45:27 +0700

    init

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, get, child } from "firebase/database"; // Убедитесь, что ref импортирован
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZfj3T2vSej8mV8Y-xdG21pAAg24DDVe4",
-  authDomain: "sunvillage-6aec8.firebaseapp.com",
-  projectId: "sunvillage-6aec8",
-  storageBucket: "sunvillage-6aec8.firebasestorage.app",
-  messagingSenderId: "117813033135",
-  appId: "1:117813033135:web:5a84e59d4d73fdae037c5a",
-  measurementId: "G-9D6CD5C887",
-  databaseURL: "https://sunvillage-6aec8-default-rtdb.europe-west1.firebasedatabase.app/",
+
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 };
 
 // Initialize Firebase
